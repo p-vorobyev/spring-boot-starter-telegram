54fd1264e9c3b1ab589bd0b68e01c65f7a70afa5
Pavel Vorobyev <p.vorobyev@inbox.ru>
2025-01-25 22:43:33 +0300

    spring boot version updated

--- spring-boot-starter-telegram/src/test/java/dev/voroby/springframework/telegram/AbstractTest.java
+++ spring-boot-starter-telegram/src/test/java/dev/voroby/springframework/telegram/AbstractTest.java
@@ -2,12 +2,12 @@ package dev.voroby.springframework.telegram;
 
 import dev.voroby.springframework.telegram.client.TelegramClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest(classes = {TelegramClientAutoConfiguration.class})
 public abstract class AbstractTest {
 
-    @MockBean
+    @MockitoBean
     public TelegramClient telegramClient;
 
 }
